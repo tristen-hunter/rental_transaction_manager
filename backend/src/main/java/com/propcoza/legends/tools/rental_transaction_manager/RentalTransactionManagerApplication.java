d183237783eaf0bd96358c289db1fbf026728916
Tristen <tristen.hunter.dev@gmail.com>
2026-04-21 11:10:45 +0200

    feat: DB schema's defined

---
+++ backend/src/main/java/com/propcoza/legends/tools/rental_transaction_manager/RentalTransactionManagerApplication.java
@@ -0,0 +1,13 @@
+package com.propcoza.legends.tools.rental_transaction_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalTransactionManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalTransactionManagerApplication.class, args);
+	}
+
+}
