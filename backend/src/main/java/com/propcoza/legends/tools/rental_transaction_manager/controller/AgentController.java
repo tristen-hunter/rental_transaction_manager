7875b814a3d393d6cb2d5a8b5d28fcae1d32b86f
Tristen <tristen.hunter.dev@gmail.com>
2026-04-21 11:12:36 +0200

    feat: first endpoint for Agent creation

---
+++ backend/src/main/java/com/propcoza/legends/tools/rental_transaction_manager/controller/AgentController.java
@@ -0,0 +1,28 @@
+package com.propcoza.legends.tools.rental_transaction_manager.controller;
+
+import com.propcoza.legends.tools.rental_transaction_manager.dto.AgentCreateDto;
+import com.propcoza.legends.tools.rental_transaction_manager.dto.AgentReturnDto;
+import com.propcoza.legends.tools.rental_transaction_manager.service.AgentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/agents")
+@RequiredArgsConstructor
+public class AgentController {
+
+    private final AgentService agentService;
+
+    @PostMapping
+    public ResponseEntity<AgentReturnDto> createAgent(@Valid @RequestBody AgentCreateDto agentCreateDto){
+        AgentReturnDto createdAgent = agentService.createAgent(agentCreateDto);
+        return new ResponseEntity<>(createdAgent, HttpStatus.CREATED);
+    }
+}
